5d563a2342c1eb67be464b3aa0322c8c2e54bbce
Rishabh Gupta <rishabhgupta0262@gmail.com>
2023-03-29 01:28:13 +0530

    Here link is tempt to invlid. and state.error is updated from <blank> -to- newErrorMessage. & rendered on the render().

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -6,12 +6,14 @@ class PostList extends Component {
       super(props)
     
       this.state = {
-         posts : []
+         posts : [],
+         errorMsg : ""
       }
     }
 
     componentDidMount() {
-        axios.get("https://jsonplaceholder.typicode.com/posts")
+        // Invalid Link for the API...
+        axios.get("https://jsonplaceholder.typicode.com/postsx")
         .then(response => {
             console.log(response)       // form here we can verify what we get as response.
             this.setState({
@@ -20,19 +22,26 @@ class PostList extends Component {
         })
         .catch(error => {
             console.log(error)
+            this.setState({
+                errorMsg : "Error in Retrieving Data!"
+            })
         })
     }
 
   render() {
-    const {posts} = this.state
+    const {posts, errorMsg} = this.state
     return (
       <div>
         List of posts : 
         {
             posts.length ? 
-            (posts.map(post => <div key={post.id}> {post.title} </div> )) :
-            ('error')
+            posts.map(post => <div key={post.id}> {post.title} </div> ) : null
         }
+
+        {
+            errorMsg ? <div> {errorMsg} </div> : null
+        }
+
       </div>
     )
   }
