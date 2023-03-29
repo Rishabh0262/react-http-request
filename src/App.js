1fa6868e906485f761cf9f14c7999b0a7e0cdf52
Rishabh Gupta <rishabhgupta0262@gmail.com>
2023-03-29 22:09:44 +0530

    Here we have created a simple FORM with values typed were dynamically stored in the respective 'state(s)'. Then we have logged all the values to the console. NEXT we will POST data to the API via 'axios'.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import PostList from './components/PostList'
+import PostForm from './components/PostForm';
 
 function App() {
   return (
     <div className="App">
 
-      <PostList />          
+      <PostForm />         
 
     </div>
   );
---
+++ src/components/PostForm.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+class PostForm extends Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         userId : "",
+         title : "",
+         body : ""
+      }
+    }
+
+    changeHandler = e => {
+        this.setState({
+            [e.target.name] : e.target.value
+        })
+
+    }
+
+    submitHandler = (e) => {
+        e.preventDefault()
+        console.log(this.state)
+    }
+
+
+  render() {
+    const {userId, title, body} = this.state
+    return (
+      <div>
+        <form onSubmit={this.submitHandler}>
+            <label>
+                User ID : &nbsp;
+                <input type="text" name="userId" value={userId} onChange={this.changeHandler} />
+            </label><br></br>
+            <label>
+                Title : &nbsp;
+                <input type="text" name="title" value={title} onChange={this.changeHandler} />
+            </label><br></br>
+            <label>
+                Body : &nbsp;
+                <input type="text" name="body" value={body} onChange={this.changeHandler} />
+            </label>
+            <br />
+            <button type='submit' >Post Data</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default PostForm
